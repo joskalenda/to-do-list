9daeb15b706b992719799dd38008fb8f9e84b14a
Topaz Jos <topazjos@Topaz.local>
2022-01-12 00:40:40 +0200

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -12,7 +12,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
-      template: './src/module/index.html',
+      template: './src/index.html',
     }),
   ],
   module: {
