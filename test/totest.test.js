9252c678a8e831e18e992e36c06713b6dcecb016
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-21 22:55:14 +0200

    Add the jest test >> Test to remove completed task a task item

--- test/totest.test.js
+++ test/totest.test.js
@@ -1,4 +1,4 @@
-import { createTodo, removeTodoItem, editTodoItem } from '../src/script.js';
+import { createTodo, removeTodoItem, editTodoItem, removeCompleted } from '../src/script.js';
 import { savedListData } from '../src/savelist.js';
 import setCompleted from '../src/marked.js';
 import * as localStorage from '../src/savelist.js';
@@ -74,5 +74,50 @@ describe('Manipulating the DOM', () => {
     expect(ToSetCompleted.index).toBe(1);
     expect(ToSetCompleted.description).toBe('Colors');
     expect(ToSetCompleted.completed).toBe(true);
+  });
+  t('This remove completed task', () => {
+    // Arrange
+    const StartingList = `
+      <div id="0" class="task--div"></div>
+      <div id="1" class="task--div"></div>
+      <div id="2" class="task--div"></div>
+    `;
+    document.querySelector('.main .section--task').innerHTML = StartingList;
+    // mock todoLit items
+    localStorage.todoList = {
+      data: [
+        {
+          // this item should be removed
+          description: 'Item 1',
+          completed: true,
+          index: 0,
+        },
+        {
+          description: 'Item 2',
+          completed: false,
+          index: 1,
+        },
+        {
+          description: 'Item 3',
+          completed: false,
+          index: 2,
+        },
+      ],
+    };
+    // Act
+    removeCompleted();
+    // Assert
+    const result = document.querySelectorAll('.task--div');
+    expect(result).toHaveLength(2); //
+    expect(result[0].id).toBe('0');
+    expect(result[1].id).toBe('1');
+    // two should remain in the todoList
+    expect(localStorage.todoList.data).toHaveLength(2);
+    // Item 2 should remain and be unmodified
+    expect(localStorage.todoList.data[0].description).toBe('Item 2');
+    expect(localStorage.todoList.data[0].completed).toBe(false);
+    // Item 2 should remain and be unmodified
+    expect(localStorage.todoList.data[1].description).toBe('Item 3');
+    expect(localStorage.todoList.data[1].completed).toBe(false);
   });
 });
