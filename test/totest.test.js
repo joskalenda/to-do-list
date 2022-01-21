334e7a9325042a8e45bd8c5094086f45a15eddc5
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-21 11:35:22 +0200

    Add the jest test >> Test to remove a task item

--- test/totest.test.js
+++ test/totest.test.js
@@ -1,4 +1,4 @@
-import { createTodo } from '../src/script.js';
+import { createTodo, removeTodoItem } from '../src/script.js';
 
 describe('Manipulating the DOM', () => {
   it('This should add list to THE DOM', () => {
@@ -17,5 +17,20 @@ describe('Manipulating the DOM', () => {
     const listToAdd = document.querySelectorAll('.task--div');
     // Assert
     expect(listToAdd).toHaveLength(2);
+  });
+
+  it('should remove an item from the dom', () => {
+    const StartingList = '';
+    document.querySelector('.main .section--task').innerHTML = StartingList;
+    const sample = {
+      description: 'TEST 2 ADDED TO LIST',
+      index: 1,
+      completed: false,
+    };
+    createTodo(sample);
+    const listElement = document.querySelector('.task--div');
+    removeTodoItem(listElement);
+    const listToRemove = document.querySelectorAll('.task--div');
+    expect(listToRemove).toHaveLength(0);
   });
 });
